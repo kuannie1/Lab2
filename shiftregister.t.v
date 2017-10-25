14f01b547ae0548d517a21aefb9df783822a0fd7
kuannie1 <anne.ku4@gmail.com>
2017-10-25 17:13:21 -0400

    progress for 10/25

--- shiftregister.t.v
+++ shiftregister.t.v
@@ -1,6 +1,8 @@
 //------------------------------------------------------------------------
 // Shift Register test bench
 //------------------------------------------------------------------------
+`include "shiftregister.v"
+
 
 module testshiftregister();
 
@@ -21,8 +23,34 @@ module testshiftregister();
     		           .parallelDataOut(parallelDataOut), 
     		           .serialDataOut(serialDataOut));
     
+
+    // Generate clock (50MHz)
+    initial begin 
+    clk=0;
+    end
+    always #10 clk=!clk;    // 50MHz Clock
+
+
     initial begin
     	// Your Test Code
+        $dumpfile("shiftregister.vcd");
+        $dumpvars();
+
+        parallelLoad = 1; peripheralClkEdge = 0; parallelDataIn = 8'b11111111; #20
+        if ((parallelDataOut != parallelDataIn) && (serialDataOut != 1)) $display("Test 1 Failed");
+
+        parallelLoad = 0; peripheralClkEdge = 1; serialDataIn = 1'b0; #20
+        if ((parallelDataOut != 8'b11111110) && (serialDataOut != 1)) $display("Test 2 Failed");
+
+
+        parallelLoad = 1; peripheralClkEdge = 1; parallelDataIn = 8'b00000000; serialDataIn = 1'b1; #20
+        if ((parallelDataOut != 8'b00000000) && (serialDataOut != 0)) $display("Test 3 Failed");
+
+        parallelLoad = 0; peripheralClkEdge = 1; serialDataIn = 1'b1; #20
+        if ((parallelDataOut != 8'b00000001) && (serialDataOut != 0)) $display("Test 4 Failed");
+
+        #100 $finish;
+
     end
 
 endmodule
