961bfbf52938ef7e242f6e790ddb93787e14defd
Serena Chen <serenazychen@gmail.com>
2017-11-01 16:28:36 -0400

    input conditioner tests automated

--- inputconditioner.t.v
+++ inputconditioner.t.v
@@ -1,7 +1,23 @@
 //------------------------------------------------------------------------
 // Input Conditioner test bench
+// Because this is so dependent on time, we test the outputs
+// of the input conditioner every 20 time units (one clock cycle)
 //------------------------------------------------------------------------
 `include "inputconditioner.v"
+
+module outputTester (
+    input conditioned,
+    input rising,
+    input falling,
+    input expectedCond,
+    input expectedRise,
+    input expectedFall,
+    output equivalent
+);
+
+assign equivalent = (conditioned == expectedCond) & (rising == expectedRise) & (falling == expectedFall);
+
+endmodule
 
 
 module testConditioner();
@@ -18,27 +34,124 @@ module testConditioner();
 			 .positiveedge(rising),
 			 .negativeedge(falling));
 
+    reg expectedCond, expectedRise, expectedFall;
+    wire isEquivalent;
+    outputTester tester(.conditioned(conditioned),
+        .rising(rising),
+        .falling(falling),
+        .expectedCond(expectedCond),
+        .expectedRise(expectedRise),
+        .expectedFall(expectedFall),
+        .equivalent(isEquivalent));
 
     // Generate clock (50MHz)
     initial begin 
     clk=0;
+    pin=0;
     end
     always #10 clk=!clk;    // 50MHz Clock
     
     initial begin
     $dumpfile("iconditioner.vcd");
     $dumpvars();
-    pin=1; #200
 
-    pin=0; #200
+    pin=1;
 
-    pin=1; #30
+    expectedCond = 0;
+    expectedRise = 0;
+    expectedFall = 0;
+    repeat (3) begin
+        #20
+        if (isEquivalent == 0) begin
+            $display("Test 1 failed: expected %b %b %b, got %b %b %b", 
+                expectedCond, expectedRise, expectedFall, conditioned, rising, falling);
+        end
+    end
+    
+    expectedCond = 1;
+    expectedRise = 1;
+    #20
+    if (isEquivalent == 0) begin
+        $display("Test 2 failed: expected %b %b %b, got %b %b %b", 
+            expectedCond, expectedRise, expectedFall, conditioned, rising, falling);
+    end
 
-    pin=0; #30
+    expectedRise = 0;
+    #20
+    if (isEquivalent == 0) begin
+        $display("Test 3 failed: expected %b %b %b, got %b %b %b", 
+            expectedCond, expectedRise, expectedFall, conditioned, rising, falling);
+    end
 
-    pin=1; #50
+    #40
 
-    #100 $finish;
+    pin=0;
+
+    repeat (5) begin
+        #20
+        if (isEquivalent == 0) begin
+            $display("Test 4 failed: expected %b %b %b, got %b %b %b", 
+                expectedCond, expectedRise, expectedFall, conditioned, rising, falling);
+        end
+    end
+
+    expectedCond = 0;
+    expectedFall = 1;
+    #20
+    if (isEquivalent == 0) begin
+        $display("Test 5 failed: expected %b %b %b, got %b %b %b", 
+            expectedCond, expectedRise, expectedFall, conditioned, rising, falling);
+    end
+
+    expectedFall = 0;
+    #20
+    if (isEquivalent == 0) begin
+        $display("Test 6 failed: expected %b %b %b, got %b %b %b", 
+            expectedCond, expectedRise, expectedFall, conditioned, rising, falling);
+    end
+
+    pin=1;
+
+    #20
+    if (isEquivalent == 0) begin
+        $display("Test 7 failed: expected %b %b %b, got %b %b %b", 
+            expectedCond, expectedRise, expectedFall, conditioned, rising, falling);
+    end
+
+    pin=0;
+
+    #20
+    if (isEquivalent == 0) begin
+        $display("Test 8 failed: expected %b %b %b, got %b %b %b", 
+            expectedCond, expectedRise, expectedFall, conditioned, rising, falling);
+    end
+
+    pin=1;
+
+    repeat (5) begin
+        #20
+        if (isEquivalent == 0) begin
+            $display("Test 9 failed: expected %b %b %b, got %b %b %b", 
+                expectedCond, expectedRise, expectedFall, conditioned, rising, falling);
+        end
+    end
+
+    expectedCond = 1;
+    expectedRise = 1;
+    #20
+    if (isEquivalent == 0) begin
+        $display("Test 10 failed: expected %b %b %b, got %b %b %b", 
+            expectedCond, expectedRise, expectedFall, conditioned, rising, falling);
+    end
+
+    expectedRise = 0;
+    #20
+    if (isEquivalent == 0) begin
+        $display("Test 11 failed: expected %b %b %b, got %b %b %b", 
+            expectedCond, expectedRise, expectedFall, conditioned, rising, falling);
+    end
+
+    $finish;
     end
     
 endmodule
